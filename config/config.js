c0d75eabbe4905d665a1d0e697816262e21e91d1
Rossario Catherine Elfrida <roselfridaa@gmail.com>
2024-04-17 19:42:20 +0800

    remove unused syntax

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ const config = {
     port: process.env.PORT || 80
   },
   jwt: {
-    key: process.env.JWT_SECRET_KEY || JWT_SECRET_KEY
+    key: process.env.JWT_SECRET_KEY
   },
   api: {
-    key: process.env.GOOGLE_MAPS_API_KEY || GOOGLE_MAPS_API_KEY
+    key: process.env.GOOGLE_MAPS_API_KEY
   }
 }
 
