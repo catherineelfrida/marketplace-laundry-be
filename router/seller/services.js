a77c2a75dddcf4caa73cdab11ec421ade7ceae75
Rossario Catherine Elfrida <roselfridaa@gmail.com>
2024-05-26 21:06:50 +0800

    ubah sellerservices jadi sellerServices

--- router/seller/services.js
+++ router/seller/services.js
@@ -4,10 +4,10 @@ const { auth } = require('../../middleware/jwt')
 
 const router = express.Router()
 
-router.get('/services', auth, controller.sellerservices.get)
-router.get('/services/:id', auth, controller.sellerservices.getById)
-router.post('/services', auth, controller.sellerservices.create)
-router.put('/services/:id', auth, controller.sellerservices.update)
-router.delete('/services/:id', auth, controller.sellerservices.destroy)
+router.get('/services', auth, controller.sellerServices.get)
+router.get('/services/:id', auth, controller.sellerServices.getById)
+router.post('/services', auth, controller.sellerServices.create)
+router.put('/services/:id', auth, controller.sellerServices.update)
+router.delete('/services/:id', auth, controller.sellerServices.destroy)
 
 module.exports = router
